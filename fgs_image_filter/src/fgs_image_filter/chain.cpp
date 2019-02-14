1abd61192a4aa03d714abea7a367a4dd8de6156b
fugashy <fugashy@icloud.com>
2019-02-14 21:10:12 +0900

    Modify to use service(unstable)

--- fgs_image_filter/src/fgs_image_filter/chain.cpp
+++ fgs_image_filter/src/fgs_image_filter/chain.cpp
@@ -27,7 +27,7 @@ Chain::Chain(ros::NodeHandle& nh) {
     if (i == 0) {
       remap["/camera/image_raw"] = input_topic;
     } else {
-      std::string last_nodelet_name = manager_->listLoadedNodelets().back();
+      const std::string last_nodelet_name = manager_->listLoadedNodelets().back();
       remap["/camera/image_raw"] = last_nodelet_name + "/image_filtered";
     }
 
@@ -39,7 +39,7 @@ Chain::Chain(ros::NodeHandle& nh) {
     manager_->load(sout.str(), "fgs_image_filter/Nodelet", remap, argv);
   }
 
-  auto nodelet_list = manager_->listLoadedNodelets();
+  const auto nodelet_list = manager_->listLoadedNodelets();
   for (int i = 0, iend = nodelet_list.size(); i < iend; ++i) {
     ROS_INFO("Loaded node[%d]: %s", i, nodelet_list[i].c_str());
   }
@@ -48,18 +48,40 @@ Chain::Chain(ros::NodeHandle& nh) {
 
 bool Chain::ChangeChainNum(fgs_image_filter::ChangeChainNum::Request& req,
                            fgs_image_filter::ChangeChainNum::Response& res) {
+  const int current_chain_num = manager_->listLoadedNodelets().size();
+  ROS_INFO("current_chain_num : %d", current_chain_num);
+  const int diff = req.num - current_chain_num;
+  ROS_INFO("diff : %d", diff);
   if (req.num <= 0) {
     ROS_WARN("Number of chain should be larger than 0.");
     return false;
+  } else if (diff == 0) {
+    ROS_WARN("Requested chain num is same as current chain num. We ignore this request.");
+    return false;
+  } else if (req.num >= 5) {
+    ROS_WARN("Number of chain should be less than 5.(Limited by number of worker thread)");
+    return false;
   }
-  manager_.reset();
 
-  for (int i = 0, iend = req.num; i < iend; ++i) {
-    nodelet::M_string remap;
-    nodelet::V_string argv;
-    std::ostringstream sout;
-    sout << "filter_" << std::setfill('0') << i;
-    manager_->load(sout.str(), "fgs_image_filter/Nodelet", remap, argv);
+  if (diff > 0) {
+    // Load nodelets
+    for (int i = current_chain_num, iend = req.num; i < iend; ++i) {
+      nodelet::M_string remap;
+      ROS_INFO("Trying to load nodelet");
+      const std::string last_nodelet_name = manager_->listLoadedNodelets().back();
+      ROS_INFO("Name of last nodelet : %s", last_nodelet_name.c_str());
+      remap["/camera/image_raw"] = last_nodelet_name + "/image_filtered";
+      nodelet::V_string argv;
+      std::ostringstream sout;
+      sout << "filter_" << std::setfill('0') << std::setw(3) << i;
+      manager_->load(sout.str(), "fgs_image_filter/Nodelet", remap, argv);
+    }
+  } else {
+    // Unload unnecessary nodelets
+    const auto nodelet_list = manager_->listLoadedNodelets();
+    for (int i = req.num, iend = current_chain_num; i < iend; ++i) {
+      manager_->unload(nodelet_list[i]);
+    }
   }
 
   return true;
