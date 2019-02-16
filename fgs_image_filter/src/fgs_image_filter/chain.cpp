926aec34e0e8c55b686dbcefac18d6019c223f63
fugashy <fugashy@icloud.com>
2019-02-16 11:25:34 +0900

    Refactoring
    - Suppess unnecessary log
    - Constant declaration of frequently used values

--- fgs_image_filter/src/fgs_image_filter/chain.cpp
+++ fgs_image_filter/src/fgs_image_filter/chain.cpp
@@ -3,6 +3,14 @@
 #include <sstream>
 #include <iomanip>
 
+namespace {
+const char* const kTopicOfInput = "/camera/image_raw";
+const char* const kTopicOfFiltered = "/image_filtered";
+const char* const kNameOfNodelet = "fgs_image_filter/Nodelet";
+const int kMinChainNum = 1;
+const int kMaxChainNum = 8;
+}
+
 namespace fgs {
 namespace image_filter {
 
@@ -13,11 +21,11 @@ Chain::Chain(ros::NodeHandle& nh) {
 
   int chain_num;
   nh.param<int>("default_chain_num", chain_num, 3);
-  if (chain_num <= 0) {
-    throw std::runtime_error("Number of chain should be larger than 0.");
+  if (chain_num < kMinChainNum || kMaxChainNum < chain_num) {
+    throw std::runtime_error("Number of chain should be in range of 1 to 8");
   }
   std::string input_topic;
-  nh.param<std::string>("input_topic", input_topic, "/camera/image_raw");
+  nh.param<std::string>("input_topic", input_topic, kTopicOfInput);
   if (input_topic == std::string()) {
     throw std::runtime_error("Name of input topic is empty.");
   }
@@ -25,10 +33,10 @@ Chain::Chain(ros::NodeHandle& nh) {
   for (int i = 0; i < chain_num; ++i) {
     nodelet::M_string remap;
     if (i == 0) {
-      remap["/camera/image_raw"] = input_topic;
+      remap[kTopicOfInput] = input_topic;
     } else {
       const std::string last_nodelet_name = manager_->listLoadedNodelets().back();
-      remap["/camera/image_raw"] = last_nodelet_name + "/image_filtered";
+      remap[kTopicOfInput] = last_nodelet_name + kTopicOfFiltered;
     }
 
     // Generate nodelet name
@@ -36,33 +44,25 @@ Chain::Chain(ros::NodeHandle& nh) {
     sout << "filter_" << std::setfill('0') << std::setw(3) << i;
 
     nodelet::V_string argv;
-    if (!manager_->load(sout.str(), "fgs_image_filter/Nodelet", remap, argv)) {
+    if (!manager_->load(sout.str(), kNameOfNodelet, remap, argv)) {
       const std::string error_msg = std::string("Failed to load ") + sout.str();
       throw std::runtime_error(error_msg);
     }
   }
 
-  const auto nodelet_list = manager_->listLoadedNodelets();
-  for (int i = 0, iend = nodelet_list.size(); i < iend; ++i) {
-    ROS_INFO("Loaded node[%d]: %s", i, nodelet_list[i].c_str());
-  }
   ROS_INFO("Ready to filtering");
 }
 
 bool Chain::ChangeChainNum(fgs_image_filter::ChangeChainNum::Request& req,
                            fgs_image_filter::ChangeChainNum::Response& res) {
+  ROS_WARN("This service is unstable...");
+  if (req.num < kMinChainNum || kMaxChainNum < req.num) {
+    throw std::runtime_error("Number of chain should be in range of 1 to 8");
+  }
   const int current_chain_num = manager_->listLoadedNodelets().size();
-  ROS_INFO("current_chain_num : %d", current_chain_num);
   const int diff = req.num - current_chain_num;
-  ROS_INFO("diff : %d", diff);
-  if (req.num <= 0) {
-    ROS_WARN("Number of chain should be larger than 0.");
-    return false;
-  } else if (diff == 0) {
+  if (diff == 0) {
     ROS_WARN("Requested chain num is same as current chain num. We ignore this request.");
-    return false;
-  } else if (req.num >= 5) {
-    ROS_WARN("Number of chain should be less than 5.(Limited by number of worker thread)");
     return false;
   }
 
@@ -70,14 +70,12 @@ bool Chain::ChangeChainNum(fgs_image_filter::ChangeChainNum::Request& req,
     // Load nodelets
     for (int i = current_chain_num, iend = req.num; i < iend; ++i) {
       nodelet::M_string remap;
-      ROS_INFO("Trying to load nodelet");
       const std::string last_nodelet_name = manager_->listLoadedNodelets().back();
-      ROS_INFO("Name of last nodelet : %s", last_nodelet_name.c_str());
-      remap["/camera/image_raw"] = last_nodelet_name + "/image_filtered";
+      remap[kTopicOfInput] = last_nodelet_name + kTopicOfFiltered;
       nodelet::V_string argv;
       std::ostringstream sout;
       sout << "filter_" << std::setfill('0') << std::setw(3) << i;
-      if (!manager_->load(sout.str(), "fgs_image_filter/Nodelet", remap, argv)) {
+      if (!manager_->load(sout.str(), kNameOfNodelet, remap, argv)) {
         ROS_WARN("Failed to load %s", sout.str().c_str());
         return false;
       }
